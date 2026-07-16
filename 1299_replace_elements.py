e18b0b63e7556ebadac1662a66d1f28255e9bbef
Milos Arsik <milosarsik@Milos-MacBook-Pro.local>
2026-07-16 11:57:36 -0400

    Add missing array solution variants

--- 1299_replace_elements.py
+++ 1299_replace_elements.py
@@ -26,12 +26,26 @@ def replace_elements_suffix_max(arr):
     return res
 
 
+def replace_elements_in_place(arr):
+    # In-place suffix max
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
+    largest = -1
+
+    for i in range(len(arr) - 1, -1, -1):
+        arr[i], largest = largest, max(largest, arr[i])
+
+    return arr
+
+
 arr = [17, 18, 5, 4, 6, 1]
 
 print("brute force:", replace_elements_brute_force(arr.copy()))
 print("suffix max:", replace_elements_suffix_max(arr.copy()))
+print("in place:", replace_elements_in_place(arr.copy()))
 
 arr = [400]
 
 print("brute force:", replace_elements_brute_force(arr.copy()))
 print("suffix max:", replace_elements_suffix_max(arr.copy()))
+print("in place:", replace_elements_in_place(arr.copy()))
--- 1929_concatenation_of_array.py
+++ 1929_concatenation_of_array.py
@@ -13,6 +13,32 @@ def get_concatenation_brute_force(nums):
     return ans
 
 
+def get_concatenation_iteration(nums):
+    # Iteration I
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
+    length = len(nums)
+    ans = [0] * (length * 2)
+
+    for i in range(length):
+        ans[i] = ans[i + length] = nums[i]
+
+    return ans
+
+
+def get_concatenation_enumerate(nums):
+    # Iteration II
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
+    length = len(nums)
+    ans = [0] * (length * 2)
+
+    for i, num in enumerate(nums):
+        ans[i] = ans[i + length] = num
+
+    return ans
+
+
 def get_concatenation(nums):
     # Shorthand
     # Time Complexity: O(n)
@@ -23,9 +49,13 @@ def get_concatenation(nums):
 nums = [1, 2, 1]
 
 print("brute force:", get_concatenation_brute_force(nums))
+print("iteration:", get_concatenation_iteration(nums))
+print("enumerate:", get_concatenation_enumerate(nums))
 print("shorthand:", get_concatenation(nums))
 
 nums = [1, 3, 2, 1]
 
 print("brute force:", get_concatenation_brute_force(nums))
+print("iteration:", get_concatenation_iteration(nums))
+print("enumerate:", get_concatenation_enumerate(nums))
 print("shorthand:", get_concatenation(nums))
