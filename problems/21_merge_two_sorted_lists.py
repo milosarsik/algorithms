65672e1cdae7ae5dd457131be362a654304f412e
Milos Arsik <44819768+milosarsik@users.noreply.github.com>
2026-08-07 11:55:37 -0400

    Add merge two lists attempt

--- problems/21_merge_two_sorted_lists.py
+++ problems/21_merge_two_sorted_lists.py
@@ -50,6 +50,41 @@ def to_list(head):
     return values
 
 
+# My Attempt
+# Time Complexity: O(m + n)
+# Space Complexity: O(1)
+def merge_two_lists_my_attempt(list1, list2):
+    if not list1 and not list2:
+        return None
+
+    if not list1:
+        return list2
+
+    if not list2:
+        return list1
+
+    dummy = ListNode()
+    current = dummy
+
+    while list1 and list2:
+        if list1.val <= list2.val:
+            current.next = list1
+            list1 = list1.next
+        else:
+            current.next = list2
+            list2 = list2.next
+
+        current = current.next
+
+    if list1:
+        current.next = list1
+    else:
+        current.next = list2
+
+    return dummy.next
+
+
+# Optimized / Cleaned Up
 # Time Complexity: O(m + n)
 # Space Complexity: O(1)
 def merge_two_lists_iterative(list1, list2):
@@ -90,11 +125,23 @@ def merge_two_lists_recursive(list1, list2):
 
 list1 = build_linked_list([1, 2, 4])
 list2 = build_linked_list([1, 3, 4])
+print("my attempt:", to_list(merge_two_lists_my_attempt(list1, list2)))  # [1, 1, 2, 3, 4, 4]
+
+list1 = build_linked_list([1, 2, 4])
+list2 = build_linked_list([1, 3, 4])
 print("iterative:", to_list(merge_two_lists_iterative(list1, list2)))  # [1, 1, 2, 3, 4, 4]
 
 list1 = build_linked_list([])
 list2 = build_linked_list([0])
+print("my attempt:", to_list(merge_two_lists_my_attempt(list1, list2)))  # [0]
+
+list1 = build_linked_list([])
+list2 = build_linked_list([0])
 print("iterative:", to_list(merge_two_lists_iterative(list1, list2)))  # [0]
+
+list1 = build_linked_list([])
+list2 = build_linked_list([])
+print("my attempt:", to_list(merge_two_lists_my_attempt(list1, list2)))  # []
 
 list1 = build_linked_list([])
 list2 = build_linked_list([])
